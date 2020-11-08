936cd8391d82e245ae63d9568953fe8c8145fb32
camiloserr <camiloserrano@javeriana.edu.co>
2020-11-08 14:12:12 -0500

    Switch working with realtimedatabase

--- app/src/main/java/com/example/taller3/MenuActivity.java
+++ app/src/main/java/com/example/taller3/MenuActivity.java
@@ -1,14 +1,137 @@
 package com.example.taller3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.taller3.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button logout;
+    private FirebaseAuth mAuth;
+    private Switch switchAB;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private final static String TAG = "menu activity";
+    private DatabaseReference ref;
+    private User myUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        logout = findViewById(R.id.buttonLogOutMenu);
+        mAuth = FirebaseAuth.getInstance();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                updateUI(null);
+            }
+        });
+
+
+        ref = database.getReference("users").child(mAuth.getCurrentUser().getUid());
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User u = dataSnapshot.getValue(User.class);
+                Log.i(TAG, "onDataChange: " + u.getName() + " " + u.getLastname());
+                myUser = u;
+                updateSwitch();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i(TAG, "The read failed: " + databaseError.getCode());
+            }
+        });
+
+    }
+
+    private void updateSwitch() {
+        switchAB.setChecked(myUser.isAvailible());
+    }
+
+
+    private void updateUI(FirebaseUser user) {
+
+        if(user == null){
+            Intent i = new Intent(MenuActivity.this , MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        MenuItem switchOnOffItem = menu.findItem(R.id.menuItemAvailible);
+        switchOnOffItem.setActionView(R.layout.switch_layout);
+
+        switchAB = switchOnOffItem.getActionView().findViewById(R.id.mySwitch);
+        switchAB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(final CompoundButton buttonView, boolean isChecked) {
+                updateDB(isChecked);
+            }
+        });
+
+        return true;
+    }
+
+    private void updateDB(boolean isChecked) {
+        ref.child("availible").setValue(isChecked);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemClicked = item.getItemId();
+        if(itemClicked == R.id.menuItemLogout){
+            mAuth.signOut();
+            Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }else if (itemClicked == R.id.menuItemUsers){
+            Toast.makeText(getApplicationContext() , "active Users!!" , Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/taller3/SignupActivity.java
+++ app/src/main/java/com/example/taller3/SignupActivity.java
@@ -11,11 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.taller3.model.User;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -23,6 +27,7 @@ public class SignupActivity extends AppCompatActivity {
     private Button btnRegister;
     private FirebaseAuth mAuth;
     private static final String TAG = "Signup";
+    private LatLng currentLocation = new LatLng(4,-72);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +70,15 @@ public class SignupActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myRef = database.getReference("users");
+
+                            // TODO: agregar la foto a storage y cambiar currentLocation
+                            User u = new User(etID.getText().toString() , currentLocation.latitude ,
+                                    currentLocation.longitude, false,
+                                    etName.getText().toString() , etLastName.getText().toString());
+                            myRef.child(user.getUid()).setValue(u);
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -77,12 +91,12 @@ public class SignupActivity extends AppCompatActivity {
                         // ...
                     }
 
-
                 });
     }
 
 
     private void updateUI(FirebaseUser user) {
+
         if(user!=null) {
             Intent i = new Intent(SignupActivity.this, MenuActivity.class);
             startActivity(i);
