9f8dbcc3be6489e6fa7cbcad311f1c378ff60cd6
Juls <julio.mejia@javeriana.edu.co>
2020-11-10 19:53:10 -0500

    Listo foticos!

--- app/src/main/java/com/example/taller3/SignupActivity.java
+++ app/src/main/java/com/example/taller3/SignupActivity.java
@@ -1,34 +1,59 @@
 package com.example.taller3;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.taller3.model.User;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class SignupActivity extends AppCompatActivity {
 
     private EditText etName, etLastName, etEmail, etPass1, etPass2, etID;
-    private Button btnRegister;
+    private Button btnRegister, btnAlmacenamiento;
+    private ImageView imagePicked;
     private FirebaseAuth mAuth;
     private static final String TAG = "Signup";
     private LatLng currentLocation = new LatLng(4,-72);
-
+    private static int ALMACENAMIENTO = 2;
+    private static final int IMAGE_PICKER_REQUEST = 3;
+    private StorageReference mStorageRef;
+    private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +66,10 @@ public class SignupActivity extends AppCompatActivity {
         etPass2 = findViewById(R.id.editTextTextConfirm);
         etID = findViewById(R.id.editTextIDRegisterID);
         btnRegister = findViewById(R.id.buttonSignUp);
-
+        btnAlmacenamiento = findViewById(R.id.imgSignUpBtn);
+        imagePicked = findViewById(R.id.imgPickSignUp);
         mAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +82,14 @@ public class SignupActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Las contrasenas no coinciden", Toast.LENGTH_SHORT).show();
                 }
                 attemptSignUp(email, password1);
+            }
+        });
+
+        btnAlmacenamiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                solicitarPermiso((Activity) v.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE,"Necesito permisos de almacenamiento",ALMACENAMIENTO);
+                usarPermiso((Activity) v.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
             }
         });
 
@@ -79,6 +114,7 @@ public class SignupActivity extends AppCompatActivity {
                                     -72, false,
                                     etName.getText().toString() , etLastName.getText().toString());
                             myRef.child(user.getUid()).setValue(u);
+                            uploadImage(imageUri, user.getUid());
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -94,12 +130,59 @@ public class SignupActivity extends AppCompatActivity {
                 });
     }
 
+    private void uploadImage(Uri uri, String uid){
+        if(uri != null){
+         StorageReference profileRef = mStorageRef.child("users/"+uid+"/profile.jpg");
+         profileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+             @Override
+             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                 Log.i("SigUp","Se cargo la imagen");
+             }
+         }).addOnFailureListener(new OnFailureListener() {
+             @Override
+             public void onFailure(@NonNull Exception e) {
+                 Log.i("SigUp","No se cargo la imagen");
+             }
+         });
 
+        }
+    }
     private void updateUI(FirebaseUser user) {
 
         if(user!=null) {
             Intent i = new Intent(SignupActivity.this, InteresesMapActivity.class);
             startActivity(i);
+        }
+    }
+    private void solicitarPermiso(Activity context, String permiso, String justificacion, int idPermiso){
+        if (ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{permiso}, idPermiso);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
+                Toast.makeText(this, justificacion, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    private  void usarPermiso(Activity context, String permiso){
+
+        if (ContextCompat.checkSelfPermission(this, permiso) == PackageManager.PERMISSION_GRANTED) {
+            if(permiso==Manifest.permission.READ_EXTERNAL_STORAGE){
+                Intent pickImage = new Intent(Intent.ACTION_PICK);
+                pickImage.setType("image/*");
+                startActivityForResult(pickImage, IMAGE_PICKER_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case IMAGE_PICKER_REQUEST:
+                if(resultCode == RESULT_OK){
+                    imageUri = data.getData();
+                    imagePicked.setImageURI(imageUri);
+                }
+                break;
         }
     }
 }
